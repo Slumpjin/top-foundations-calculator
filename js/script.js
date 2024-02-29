555b06cc655af8d0a9a1adb48477bd6cab2fcaaa
Slumpjin <tap4302@gmail.com>
2024-02-28 21:52:52 -0600

    Add arithmetic functions

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
+}
+
+function subtract(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
+}
+
+function multiply(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
+
+function divide(leftOperand, rightOperand) {
+    return leftOperand / rightOperand;
+}
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
