9f86ff0ca8c9230b11314a3710762cc9c0795756
Slumpjin <tap4302@gmail.com>
2024-02-29 20:20:50 -0600

    Add calculator functionality

--- js/script.js
+++ js/script.js
@@ -1,17 +1,59 @@
-let leftOperand;
-let rightOperand;
-let operator;
+let leftOperand = '';
+let rightOperand = '';
+let operator = '';
+let result = '';
+
+const displayDiv = document.querySelector('.display');
+
+const numberButtons = document.querySelectorAll('.number-buttons button');
+numberButtons.forEach(button => button.addEventListener('click', (e) => {
+    let number = e.target.innerText;
+    if (operator === '') {
+        if (result) {
+            displayDiv.innerText = '';
+            result = '';
+        }
+        leftOperand += number;
+        displayDiv.innerText = leftOperand;
+    }
+    else {
+        rightOperand += number;
+        displayDiv.innerText = rightOperand;
+    }
+}));
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+    clearOperatorAndOperands();
+    displayDiv.innerText = '0';
+});
+
+const arithmeticButtons = document.querySelectorAll('.arithmetic-buttons');
+arithmeticButtons.forEach(button => button.addEventListener('click', (e) => {
+    let op = e.target.innerText;
+    if (op === '=') {
+        if (leftOperand === '' || rightOperand === '') return;
+        result = String(operate(operator, leftOperand, rightOperand));
+        displayDiv.innerText = result;
+        clearOperatorAndOperands();
+    }
+    else {
+        operator = op;
+    }
+}));
 
 function operate(operator, leftOperand, rightOperand) {
+    let leftOp = Number(leftOperand);
+    let rightOp = Number(rightOperand);
     switch(operator) {
         case '+':
-            return add(leftOperand, rightOperand);
+            return add(leftOp, rightOp);
         case '-':
-            return subtract(leftOperand, rightOperand);
+            return subtract(leftOp, rightOp);
         case '*':
-            return multiply(leftOperand, rightOperand);
+            return multiply(leftOp, rightOp);
         case '/':
-            return divide(leftOperand, rightOperand);
+            return divide(leftOp, rightOp);
     }
 }
 
@@ -31,7 +73,6 @@ function divide(leftOperand, rightOperand) {
     return leftOperand / rightOperand;
 }
 
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+function clearOperatorAndOperands() {
+    leftOperand = rightOperand = operator = '';
+}
